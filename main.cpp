00dd59fc3faa972cb5aa8c5da74c2bd91f16ab1c
Samuel Guo <samuelzguo@gmail.com>
2021-05-04 17:23:32 -0700

    v432

--- main.cpp
+++ main.cpp
@@ -70,7 +70,7 @@ int main(int argc, char *argv[]) {
 		cout << "here2" << endl;
 
 		for (int i = 0; i < size; i++) {
-			hashtable.update(minheap.getString[i], i);
+			hashtable.update(minheap.getString(i), i);
 			cout << minheap.getString(i) << ":" << minheap.getFreq(i) << " ";
 		}
 
